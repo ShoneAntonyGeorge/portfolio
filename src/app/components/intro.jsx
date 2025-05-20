7adfdfe67bd3e76805996b70a327793544d02c1a
ShoneAntonyGeorge <shone.antony.george@csr-informatik.de>
2025-05-20 19:37:15 +0530

    added horizontal scroll

---
+++ src/app/components/intro.jsx
@@ -0,0 +1,9 @@
+
+export default function Intro(){
+    
+    return(
+        <div className="bg-yellow-900 h-screen w-screen" >
+
+        </div>
+    )
+} 
---
+++ src/app/components/second.jsx
@@ -0,0 +1,9 @@
+
+export default function Second(){
+    
+    return(
+        <div className="bg-yellow-300 h-screen w-screen" >
+
+        </div>
+    )
+} 
