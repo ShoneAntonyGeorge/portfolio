7f8661383ce6e7f5a3caa063b3d823bdd00d38d5
ShoneAntonyGeorge <shone.antony.george@csr-informatik.de>
2025-05-24 17:18:51 +0530

    dealing gracefully

--- src/app/components/stacks/stacks.jsx
+++ src/app/components/stacks/stacks.jsx
@@ -5,13 +5,17 @@ import React from "react";
 import { useEffect, useRef, useState } from "react";
 
 function Stacks(){
-    const stacks = [1,2,3,4];
+    const stacks = ["React","NodeJs","Javascript","Typescript"];
+    const stackImages = ["/react.png","/node.png","/javascript.png","/typescript.png"]
     const handRef = useRef(null);
     const animated = useRef(false);
-    const initialCardClass1 = "absolute -translate-y-[150%] scale-50 rounded-md border-6  border-white bg-white h-[132px] w-[90px]";
-    const initialCardClass2 = "absolute rounded-md border-6  border-white bg-white h-[132px] w-[90px]";
-    const initialCardClass3 = "rounded-md border-6  border-white bg-white h-[132px] w-[90px]";
+    const [imageUrls,setImageUrls] = useState(Array(stacks.length).fill("/card.png"));
+    const [labelsVisible,setLabelsVisible] = useState(false);
+    const initialCardClass1 = "absolute -translate-y-[150%] scale-50";
+    const initialCardClass2 = "";
+    const initialCardClass3 = "cardFlipClass ";
     const [cardClasses,setCardClasses] = useState(initialCardClass1);
+    const [afterAnimationHand,setAfterAnimationHand] = useState('');
 
     
     useEffect(() => {
@@ -19,7 +23,10 @@ function Stacks(){
             // setTimeout(() => setCardClasses(initialCardClass1),100);
             if(!animated.current){
                 setTimeout(() => setCardClasses(initialCardClass2),600);
-                setTimeout(() => setCardClasses(initialCardClass3),1000);
+                setTimeout(() => setCardClasses(initialCardClass3),1200);
+                setTimeout(() => {setImageUrls(stackImages);setLabelsVisible(true)},2050);
+                setTimeout(() => setAfterAnimationHand('-translate-y-[100%]'),1200);
+                setTimeout(() => setAfterAnimationHand('hidden -translate-y-[50%]'),1700);
                 animated.current = true;
             }
         }
@@ -45,26 +52,30 @@ function Stacks(){
     },[])
 
     return(
-        <div className="bg-linear-to-r relative from-violet-200 to-red-200  h-screen w-screen" >
+        <div className="overflow-y-hidden bg-linear-to-r relative from-violet-200 to-red-200  h-screen w-screen" >
             <Image
                 ref={handRef}
                 src="/hands.png"
                 width={120}
                 height={120}
                 alt="hands"
-                className="absolute top-0 left-1/2 -translate-x-1/2 rotate-180 "
+                className={`${afterAnimationHand} absolute top-0 left-1/2 -translate-x-1/2 rotate-180 transition-transform duration-500 ease-in-out`}
             />
 
-            <div className="flex absolute w-full justify-center top-2/5 gap-10 -translate-y-1/2">
-                {stacks.map(stack => 
-                    <div key={stack} className={cardClasses+"transition-transform duration-300 ease-in-out"}>
-                        <Image  
-                            alt="card" 
-                            src="/card.png" 
-                            width={110} 
-                            height={110}
-                            className="h-full w-full"
-                        />
+            <div className="flex absolute w-screen flex-wrap justify-center top-2/5 gap-10 -translate-y-1/2">
+                {stacks.map((stack,index) => 
+                    <div key={stack} className={cardClasses+" flex items-center flex-col transition-transform duration-600 ease-in-out"}>
+                        <div className={" flex justify-center items-center  rounded-md border-6  border-gray-100 bg-gray-50 h-[132px] w-[90px]"}>
+                            <Image  
+                                alt="card" 
+                                src={imageUrls[index]} 
+                                width={110} 
+                                height={110}
+                                className=""
+                            />
+                            
+                        </div>
+                        <span className="mt-1 text-xl text-transparent bg-clip-text bg-gradient-to-l from-red-900 to-violet-900">{labelsVisible && stack}</span>
                     </div>
                 )}
 
