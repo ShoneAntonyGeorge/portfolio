b410636727ae5cebb76ebc31bd3e25b18bb66490
ShoneAntonyGeorge <shone.antony.george@csr-informatik.de>
2025-05-24 21:07:28 +0530

    proper responsive dealing

--- src/app/components/stacks/stacks.jsx
+++ src/app/components/stacks/stacks.jsx
@@ -14,7 +14,7 @@ function Stacks(){
     const [workingVisible,setWorkingVisible] = useState(false);
     const [labelsVisible,setLabelsVisible] = useState(false);
 
-    const initialCardClass1 = "absolute -translate-y-[100%] scale-50";
+    const initialCardClass1 = "absolute top-0 -translate-y-[45%] scale-60";
     const initialCardClass2 = "";
     const initialCardClass3 = "cardFlipClass ";
     
@@ -26,10 +26,10 @@ function Stacks(){
         const animate = () =>{
             if(!animated.current){
                 setTimeout(() => {setCardClasses(initialCardClass2);setWorkingVisible(true)},1100);
-                setTimeout(() => {setLabelsVisible(true);},2100);
+                setTimeout(() => {setLabelsVisible(true);},1800);
                 setTimeout(() => setCardClasses(initialCardClass3),1700);
                 setTimeout(() => setAfterAnimationHand('-translate-y-[100%]'),1700);
-                setTimeout(() => setAfterAnimationHand('hidden -translate-y-[50%]'),2200);
+                setTimeout(() => setAfterAnimationHand('hidden -translate-y-[100%]'),2200);
                 animated.current = true;
             }
         }
@@ -55,22 +55,22 @@ function Stacks(){
     },[])
 
     return(
-        <div className="overflow-y-hidden bg-linear-to-r relative from-violet-200 to-red-200  h-screen w-screen" >
+        <div className="overflow-y-hidden bg-linear-to-r relative from-violet-200 to-red-200 min-h-screen w-screen" >
             <Image
                 ref={handRef}
                 src="/hands.png"
                 width={120}
                 height={120}
                 alt="hands"
-                className={`${afterAnimationHand} absolute top-0 left-1/2 -translate-x-1/2 rotate-180 transition-transform duration-500 ease-in-out`}
+                className={`${afterAnimationHand} absolute top-0 left-1/2 -translate-x-1/2 transition-transform duration-500 ease-in-out`}
             />
 
-            <div className="relative flex flex-col items-center w-screen  top-2/8 -translate-y-1/2">
-                <span className="mb-10 text-2xl">{labelsVisible && "STRONG EXPERIENCE IN"} </span>
-                <div className="gap-10 top-10 flex absolute w-screen flex-wrap justify-center">
+            <div className={`relative flex flex-col items-center w-screen  ${labelsVisible ? 'top-10' :'top-30'}`}>
+                <span className="mb-5 text-2xl">{labelsVisible && "STRONG EXPERIENCE IN"} </span>
+                <div className="gap-10  flex relative w-screen flex-wrap justify-center ">
                     {strongStacks.map(stack => 
-                        <div key={stack} className={cardClasses+" flex items-center flex-col transition-transform duration-600 ease-in-out"}>
-                            <div className={" flex justify-center items-center  rounded-md border-6  border-gray-100 bg-gray-50 h-[132px] w-[90px]"}>
+                        <div key={stack} className={cardClasses+" flex items-center flex-col transition-transform duration-800 ease-in-out"}>
+                            <div className={" flex justify-center items-center  rounded-md border-6  border-gray-100 bg-gray-50 h-[100px] w-[68px]"}>
                                 <Image  
                                     alt="card" 
                                     src={labelsVisible ? `/stacks/${stack.replace(" ","").toLowerCase()}.png` : "/card.png"} 
@@ -80,17 +80,17 @@ function Stacks(){
                                 />
                                 
                             </div>
-                            <span className="mt-1 text-xl text-transparent bg-clip-text bg-gradient-to-l from-red-900 to-violet-900">{labelsVisible && stack}</span>
+                            <span className="mt-1 text-md text-transparent bg-clip-text bg-gradient-to-l from-red-900 to-violet-900">{labelsVisible && stack}</span>
                         </div>
                     )}
 
                 </div>
 
-                <span className="mb-10 absolute top-60 text-xl">{labelsVisible && "WORKING KNOWLEDGE OF"} </span>
-                <div className="gap-10 top-70 flex absolute w-screen flex-wrap justify-center">
+                <span className="mb-5 relative top-10 text-xl">{labelsVisible && "WORKING KNOWLEDGE OF"} </span>
+                <div className="gap-10 top-10 mb-30 flex relative w-screen flex-wrap justify-center">
                     {workingStacks.map((stack,index) => 
                         <div key={stack} className={cardClasses+" flex items-center flex-col transition-transform duration-600 ease-in-out"}>
-                            <div className={` flex justify-center items-center  rounded-md border-6  border-gray-100 bg-gray-50 ${!workingVisible && "hidden"} ${labelsVisible ? 'h-[88px] w-[60px]' : 'h-[132px] w-[90px]'}`}>
+                            <div className={` flex justify-center items-center  rounded-md border-6  border-gray-100 bg-gray-50 ${!workingVisible && "hidden"} ${labelsVisible ? 'h-[88px] w-[60px]' : 'h-[100px] w-[68px]'}`}>
                                 <Image  
                                     alt="card" 
                                     src={labelsVisible ? `/stacks/${stack.replace(" ","").toLowerCase()}.png` : "/card.png"} 
@@ -100,7 +100,7 @@ function Stacks(){
                                 />
                                 
                             </div>
-                            <span className="mt-1 text-lg text-transparent bg-clip-text bg-gradient-to-l from-red-900 to-violet-900">{labelsVisible && stack}</span>
+                            <span className="mt-1 text-xs text-transparent bg-clip-text bg-gradient-to-l from-red-900 to-violet-900">{labelsVisible && stack}</span>
                         </div>
                     )}
 
