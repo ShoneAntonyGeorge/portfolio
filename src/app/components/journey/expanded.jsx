11b34907182e89b07d716784792d8421e3e78b3e
ShoneAntonyGeorge <shone.antony.george@csr-informatik.de>
2025-05-27 21:01:03 +0530

    added title to expand

--- src/app/components/journey/expanded.jsx
+++ src/app/components/journey/expanded.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 
-function Expanded({setExpand}){
+function Expanded({setExpand,expand}){
 
     return(
         <div className="h-screen w-screen flex fixed top-0 justify-center items-center z-1000 expandSlideClass">
-            <div className="bg-white rounded-3xl shadow-lg w-4/5 h-6/7 z-10 relative">
-                <span onClick={() => setExpand('')}>Close</span>
+            <div className="bg-white rounded-3xl shadow-lg w-4/5 h-6/7 z-10 relative flex">
+                <span onClick={() => setExpand('')} className="absolute right-0 top-0">Close</span>
+                
+                <div className="h-full w-3/10 rounded-l-3xl border-r-3 border-red-200 flex items-center p-5">
+                    <span className="text-6xl w-6/10 font-bold">{expand}</span>
+                </div>
+
+                <div className="h-full w-7/10 rounded-r-3xl">
+
+                
+                </div>
             </div>
         </div>
     )
