de52b27e0d9bb9ff3ba4825d4a0e199701272acd
ShoneAntonyGeorge <shone.antony.george@csr-informatik.de>
2025-05-27 19:39:55 +0530

    brief data added

---
+++ src/app/components/journey/brief.jsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import React from "react";
+
+
+function Brief({title,image}){
+    
+    return( 
+        <div className="relative w-[50%] h-[100px] bg-white/90 rounded-xl shadow-lg my-10 flex">
+            <div className=" w-[130px] h-[130px] absolute bg-white border-2 border-gray-200 bottom-5 rounded-md shadow-lg -left-5">
+                <Image
+                    src={`/${image}`}
+                    height={130}
+                    width={130}
+                    alt={title+" image"}
+                    className="h-full w-full"
+                />
+            </div>
+            <div className="pl-[150px] w-12/13 h-full rounded-l-xl flex flex-col justify-center">
+                <div>
+                    <span className="text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-red-900 to-red-700">
+                        {title}
+                    </span>
+                </div>
+            </div>
+            <div className="border-l-1 rounded-r-xl border-gray-200/60 hover:shadow-lg bg-gray-50/80 h-full w-1/13 flex items-center justify-center">
+                <svg className="-rotate-90  bg-violet-100 size-7 rounded-full border-1 border-gray-200" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+                    <title>Show More</title>
+                </svg>
+            </div>
+        </div>
+    )
+}
+
+export default React.memo(Brief);
---
+++ src/app/components/journey/journeyData.js
@@ -0,0 +1,14 @@
+export default  [
+    {
+        title:"Education",
+        image:"books.jpg"
+    },
+    {
+        title:"Java Tutor At Sevendyne",
+        image:"sevendyne.jpg"
+    },
+    {
+        title:"Full Stack Developer At CSR-Informatik",
+        image:"csr.svg"
+    }
+];
