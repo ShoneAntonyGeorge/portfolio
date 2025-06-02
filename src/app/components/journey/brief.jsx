dc6fa670a2c5b08539b2cb13aa0d39b7888a47b1
ShoneAntonyGeorge <shone.antony.george@csr-informatik.de>
2025-06-02 18:48:47 +0530

    brief respond to mobile

--- src/app/components/journey/brief.jsx
+++ src/app/components/journey/brief.jsx
@@ -5,8 +5,8 @@ import React from "react";
 function Brief({journey,expand,setExpand}){
     
     return( 
-        <div className={`${expand && 'journeySlideClass'} relative w-[50%] h-[100px] bg-white/90 rounded-xl shadow-lg my-10 flex`}>
-            <div className=" w-[130px] h-[130px] absolute bg-white border-2 border-gray-200 bottom-5 rounded-md shadow-lg -left-5">
+        <div className={`${expand && 'journeySlideClass'} relative w-8/10 md:w-[50%] h-[100px] bg-white/90 rounded-xl shadow-lg my-10 flex`}>
+            <div className="h-[50px] w-[50px] md:w-[130px] md:h-[130px] absolute bg-white border-2 border-gray-200 bottom-5 rounded-md shadow-lg -left-5">
                 <Image
                     src={`/${journey.image}`}
                     height={130}
@@ -15,15 +15,15 @@ function Brief({journey,expand,setExpand}){
                     className="h-full w-full"
                 />
             </div>
-            <div className="pl-[150px] w-12/13 h-full rounded-l-xl flex flex-col justify-center">
+            <div className="pl-[50px] md:pl-[150px] w-12/13  h-full rounded-l-xl flex flex-col justify-center">
                 <div>
-                    <span className="text-2xl font-medium text-transparent bg-clip-text bg-gradient-to-r from-red-900 to-red-700">
+                    <span className="md:text-2xl font-medium text-transparent bg-clip-text bg-gradient-to-r from-red-900 to-red-700">
                         {journey.title}
                     </span>
                 </div>
             </div>
-            <div onClick={() =>setExpand(journey)} className="border-l-1 rounded-r-xl border-gray-200/60 hover:shadow-lg bg-gray-50/80 h-full w-1/13 flex items-center justify-center">
-                <svg className="-rotate-90  bg-violet-100 size-7 rounded-full border-1 border-gray-200" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+            <div onClick={() =>setExpand(journey)} className="border-l-1 rounded-r-xl border-gray-200/60 hover:shadow-lg bg-gray-50/80 h-full w-2/13 md:w-1/13 flex items-center justify-center">
+                <svg className="-rotate-90  bg-violet-100 size-5 md:size-7 rounded-full border-1 border-gray-200" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
                     <title>Show More</title>
                 </svg>
