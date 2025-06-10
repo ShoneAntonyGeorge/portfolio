2d6e2de134cd0269249e0f9d12989cbbd037d577
ShoneAntonyGeorge <shone.antony.george@csr-informatik.de>
2025-06-10 22:18:15 +0530

    proper intro

--- src/app/components/intro/intro.jsx
+++ src/app/components/intro/intro.jsx
@@ -1,25 +1,40 @@
+"use client";
 import Image from "next/image";
 import IntroCard from "./introcard";
 import Socials from "./socials";
+import { useEffect } from "react";
 
 export default function Intro(){
+
+    useEffect(() => {
+        const image = new window.Image();
+        image.src = "/garden.png";
+        image.onload = () => {
+            console.log(image.width/image.height,image.height,image.width)
+        }
+    },[])
     
     return(
         <div className="bg-white w-screen relative" >
 
-            <div className="relative md:absolute top-20 md:top-0 right-0  md:translate-x-4/7">
+            <div className="flex flex-col-reverse md:block">
+                <div className=" w-screen md:w-[58vw] md:ml-25 flex justify-center md:h-screen">
+                    <IntroCard/>
+                </div>
 
-                <Image
-                src="/garden.png"
-                alt="garden"
-                width={800}
-                height={800}
-                className="w-screen h-auto md:h-screen md:w-auto "
-                />
+                <div className="relative md:absolute w-screen h-auto md:ml-[calc(100px+58vw)] md:h-[100vh] md:w-[150vh] top-20 md:top-0">
 
+                    <Image
+                        src="/garden.png"
+                        alt="garden"
+                        width={1800}
+                        height={1800}
+                        className="h-full w-full"
+                    />
+
+                </div>
             </div>
 
-            <IntroCard/>
 
             <Socials/>
         </div>
