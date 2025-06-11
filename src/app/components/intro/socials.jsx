b2dc4a54e148db8e8795b9bd1199c0bf3e29db02
ShoneAntonyGeorge <shone.antony.george@csr-informatik.de>
2025-06-11 21:17:44 +0530

    simplified socials

--- src/app/components/intro/socials.jsx
+++ src/app/components/intro/socials.jsx
@@ -2,19 +2,31 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Socials(){
+    const iconSize = 70;
+
+    const iconData = [
+        {
+            name: "instagram",
+            href: "https://www.instagram.com/shoneantonygeorge/"
+        },
+        {
+            name: "linkedin",
+            href: "https://www.linkedin.com/in/shone-antony-george-b17922299/"
+        },
+        {
+            name: "github",
+            href: "https://github.com/ShoneAntonyGeorge/portfolio"
+        }
+    ]
     return(
         <div className={`bg-[#f9bc57] md:pb-[30vh] md:pt-10 w-full md:pr-0 pr-25 md:w-md_socials_width md:h-full h-socials-height absolute top-0 flex md:flex-col justify-between items-center`}>
-            <Link href="https://www.instagram.com/shoneantonygeorge/" target="_blank">
-                <Image className="hover:drop-shadow-lg" src="/instagram.png" width={70} height={70} alt="instagram"/>
-            </Link>
 
-            <Link href="https://www.linkedin.com/in/shone-antony-george-b17922299/" target="_blank">
-                <Image className="hover:drop-shadow-lg" src="/linkedin.png" width={70} height={70} alt="linkedin"/>
-            </Link>
+            {iconData.map(icon => (
+                <Link href={icon.href} target="_blank">
+                    <Image className="hover:drop-shadow-lg" src={`/${icon.name}.png`} width={iconSize} height={iconSize} alt={icon.name}/>
+                </Link>
+            ))}
 
-            <Link href="https://github.com/ShoneAntonyGeorge/portfolio" target="_blank">
-                <Image className="hover:drop-shadow-lg" src="/github.png" width={70} height={70} alt="github"/>
-            </Link>
         </div>
     )
 }
