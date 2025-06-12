b328d9f2f72864f1c09d632f0de4448c6aa06619
ShoneAntonyGeorge <shone.antony.george@csr-informatik.de>
2025-06-12 19:54:19 +0530

    prettified floating menu

--- src/app/components/floatingMenu/floatingMenu.jsx
+++ src/app/components/floatingMenu/floatingMenu.jsx
@@ -1,22 +1,25 @@
 import { pageDatas } from "../../data/pageDatas";
 
-export default function FloatingMenu({page, updatePage}){
-    return(
-        <div className="w-screen fixed bottom-10 md:flex justify-center hidden z-10">
-            <div className="w-2/5 h-8 bg-black/40 rounded-2xl absolute top-0 shadow-xl flex justify-between px-7">
-                {pageDatas.map((PageData,index) => {
-                    const label = PageData[0];
-                    return (
-                        <button 
-                            className={(page === index ? 'underline' : '') + ' text-xs lg:text-base w-[100px] italic text-white'} 
-                            key={index}
-                            onClick={() => updatePage(index)}
-                        >
-                            {label}
-                        </button>
-                    )
-                })}
-            </div>
-        </div>
-    )
+export default function FloatingMenu({ page, updatePage }) {
+  return (
+    <div className="fixed bottom-10 z-10 hidden w-screen justify-center md:flex">
+      <div className="absolute top-0 flex h-8 w-2/5 justify-between rounded-2xl bg-black/40 px-7 shadow-xl">
+        {pageDatas.map((PageData, index) => {
+          const label = PageData[0];
+          return (
+            <button
+              className={
+                (page === index ? "underline" : "") +
+                " w-[100px] text-xs text-white italic lg:text-base"
+              }
+              key={index}
+              onClick={() => updatePage(index)}
+            >
+              {label}
+            </button>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
