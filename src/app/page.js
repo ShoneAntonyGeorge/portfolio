38bfa6028c2460508332897bf3b03a3bb033bf47
ShoneAntonyGeorge <shone.antony.george@csr-informatik.de>
2025-06-20 22:48:00 +0530

    swipe cleaned

--- src/app/page.js
+++ src/app/page.js
@@ -92,12 +92,12 @@ export default function Home(){
     }
   },[isMobileView])
 
-  const start = useRef({ x: 0, y: 0 });
+  const swipeStart = useRef({ x: 0, y: 0 });
 
   const handleTouchStart = (e) => {
     if(expand) return;
 
-    start.current = {
+    swipeStart.current = {
       x: e.touches[0].clientX,
       y: e.touches[0].clientY,
     };
@@ -111,28 +111,18 @@ export default function Home(){
       y: e.changedTouches[0].clientY,
     };
 
-    const dx = end.x - start.current.x;
-    const dy = end.y - start.current.y;
+    const dx = end.x - swipeStart.current.x;
+    const dy = end.y - swipeStart.current.y;
 
-    if (Math.abs(dx) > Math.abs(dy)) {
-      if (dx > 30){ 
-        console.log("Swipe right");
-        if(pageRef.current > 0) updatePage(pageRef.current -1 )
-      }
-      else if (dx < -30){ 
-        console.log("Swipe left");
-        if(pageRef.current < numOfPages-1) updatePage(pageRef.current + 1 )
-      }
-    } else {
-      if (dy > 30){ 
-        console.log("Swipe down");
-        if(pageRef.current > 0) updatePage(pageRef.current -1 )
-      }
-      else if (dy < -30){ 
-        console.log("Swipe up")
-        if(pageRef.current < numOfPages-1) updatePage(pageRef.current + 1 )
-      };
+    const movement = Math.abs(dx) > Math.abs(dy) ? dx : dy;
+
+    if (movement > 30 && pageRef.current > 0){ 
+      updatePage(pageRef.current -1 )
     }
+    else if (movement < -30 && pageRef.current < numOfPages-1){ 
+      updatePage(pageRef.current + 1 )
+    }
+
   };
 
 
