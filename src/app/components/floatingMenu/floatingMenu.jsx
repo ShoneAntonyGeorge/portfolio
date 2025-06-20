82a6b4538125b03b9f90b4442ebec4b512c723f0
ShoneAntonyGeorge <shone.antony.george@csr-informatik.de>
2025-06-20 23:32:20 +0530

    edited floating menu

--- src/app/components/floatingMenu/floatingMenu.jsx
+++ src/app/components/floatingMenu/floatingMenu.jsx
@@ -1,15 +1,18 @@
 import { pageDatas } from "../../data/pageDatas";
 
 export default function FloatingMenu({ page, updatePage }) {
+
+  const stackPageIndex = pageDatas.findIndex(page => page[0] === 'STACKS');
+
   return (
-    <div className="fixed bottom-10 z-10 hidden w-screen justify-center md:flex">
-      <div className="absolute top-0 flex h-8 w-2/5 justify-between rounded-2xl bg-black/40 px-7 shadow-xl">
+    <div className="fixed bottom-15 z-10 hidden w-screen justify-center md:flex">
+      <div className={`absolute top-0 flex h-8 w-2/5 justify-between rounded-2xl ${page !== stackPageIndex ? 'bg-black/80' : 'bg-black/35'} px-7 shadow-xl`}>
         {pageDatas.map((PageData, index) => {
           const label = PageData[0];
           return (
             <button
               className={
-                (page === index ? "underline" : "") +
+                (page === index ? "border-b-5 border-[#f9bc57]" : "") +
                 " w-[100px] text-xs text-white lg:text-base"
               }
               key={index}
